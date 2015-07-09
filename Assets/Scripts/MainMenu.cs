7c23191aec395026728267eaa6c46f026aaa11d2
Garrett Holaday <gholaday@vols.utk.edu>
2015-07-08 21:44:03 -0600

    Update 7-8-15
    - new main menu first parts

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+	
+	public Button[] buttons;
+	
+	
+	public float timeToAcceptInput = 4f;
+	
+	public Animator splashAnimator;
+	
+	public Image loadPanel;
+	
+	bool splashPlaying = true;
+	
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+		Invoke("AcceptInput", timeToAcceptInput);
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(splashPlaying)
+		{
+			if(Input.anyKeyDown)
+			{
+				CancelInvoke();
+				splashAnimator.speed = 50f;
+				AcceptInput();
+			}
+		}
+	
+		
+	
+	}
+	
+	void AcceptInput()	//function to be called at end of start anim
+	{
+		
+		splashPlaying = false;
+		
+		buttons[0].Select();
+		
+		foreach(Button button in buttons)
+		{
+		
+			button.interactable = true;
+		}
+		
+	}
+	
+	public void StartGame()
+	{
+		loadPanel.enabled = true;
+		loadPanel.CrossFadeAlpha(255,2f,true);
+		Invoke ("ChangeLevel", 2);
+	}
+	
+	
+	void ChangeLevel()
+	{
+		
+		Application.LoadLevel("Endless");
+		
+	}
+	
+	
+	public void StartLeaderboard()
+	{
+		Application.LoadLevel("LeaderBoardTest");
+	}
+	
+	public void DisplayOptions()
+	{
+		
+	}
+	
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
